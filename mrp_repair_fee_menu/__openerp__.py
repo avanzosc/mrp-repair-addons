0cf359997e20b7e426c9b73dd88be4dcf7a60ba7
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2015-11-19 15:38:37 +0100

    [ADD] mrp_repair_fee_menu: This module create one new menu option for "mrp.repair.fee" object.

---
+++ mrp_repair_fee_menu/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'MRP Repair Fee Menu',
+    "version": "8.0.1.0.0",
+    "license": 'AGPL-3',
+    "author": 'OdooMRP team,'
+              'AvanzOSC,'
+              'Serv. Tecnol. Avanzados - Pedro M. Baeza',
+    'website': "http://www.odoomrp.com",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+        ],
+    'category': 'Manufacturing',
+    'depends': ['base',
+                'product',
+                'stock',
+                'hr',
+                'hr_timesheet',
+                'mrp_repair',
+                'mrp_repair_analytic'
+                ],
+    'data': ['views/mrp_repair_view.xml',
+             'views/mrp_repair_fee_view.xml',
+             ],
+    'installable': True,
+}
---
+++ mrp_repair_fee_menu/tests/test_mrp_repair_fee_menu.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+import openerp.tests.common as common
+
+
+class TestMrpFeeMenu(common.TransactionCase):
+
+    def setUp(self):
+        super(TestMrpFeeMenu, self).setUp()
+        self.data_model = self.env['ir.model.data']
+        fee_vals = {'user_id': self.env.ref('base.user_root').id,
+                    'name': 'Fee line test',
+                    'product_uom': self.env.ref('product.product_uom_unit').id,
+                    'price_unit': 1,
+                    'product_uom_qty': 5}
+        vals = {'product_id': self.env.ref('product.product_product_8').id,
+                'product_uom': self.env.ref('product.product_uom_unit').id,
+                'location_id': self.env.ref('stock.stock_location_7').id,
+                'location_dest_id': self.env.ref('stock.stock_location_7').id,
+                'fees_lines': [(0, 0, fee_vals)]}
+        self.repair = self.env['mrp.repair'].create(vals)
+
+    def test_mrp_repair_fee(self):
+        self.assertEqual(
+            len(self.repair.fees_lines_no_to_invoice), 1,
+            'Repair without fee to not invoice')
+        self.repair.fees_lines_no_to_invoice[0]._onchange_user_id()
+        self.assertEqual(
+            self.repair.fees_lines_no_to_invoice[0].product_id.id,
+            self.env.ref('product.product_product_consultant').id,
+            'Wrong Product for the administrator user')
