7fee051b4264c0f27c886c068bdbbba5acf81f63
Daniel <danielcampos@avanzosc.es>
2018-06-22 14:23:41 +0200

    [ADD][8.0] mrp_repair_type (#51)

---
+++ mrp_repair_type/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "MRP Repair Type",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Daniel Campos <danielcampos@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "depends": [
+        "mrp_repair",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mrp_repair_view.xml",
+    ],
+    "installable": True,
+}
