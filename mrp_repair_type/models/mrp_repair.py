0fc5f25dd68ebd7e9900517204aca7953dcf4d2f
Daniel-CA <danielcampos@avanzosc.es>
2018-06-22 12:47:58 +0200

    [ADD][8.0] mrp_repair_type

---
+++ mrp_repair_type/models/mrp_repair.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import fields, models
+
+
+class MrpRepairType(models.Model):
+    _name = 'mrp.repair.type'
+
+    name = fields.Char(string='Repair type', help='Repair order type',
+                       translate=True)
+
+
+class MrpRepair(models.Model):
+    _inherit = 'mrp.repair'
+
+    repair_type_id = fields.Many2one(
+        comodel_name='mrp.repair.type', string='Repair order type')
