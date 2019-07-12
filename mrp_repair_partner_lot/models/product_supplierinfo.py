cb79e45603b7945aba19f5d865a7a9f444bec713
oihane <oihanecruce@gmail.com>
2019-07-12 14:14:42 +0200

    [IMP] mrp_repair_partner_lot: change name get and name_search including code and name (#57)

--- mrp_repair_partner_lot/models/product_supplierinfo.py
+++ mrp_repair_partner_lot/models/product_supplierinfo.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, api
+from openerp import api, models
+from openerp.models import expression
 
 
 class ProductSupplierinfo(models.Model):
@@ -9,13 +10,39 @@ class ProductSupplierinfo(models.Model):
 
     @api.multi
     def name_get(self):
-        if self.env.context.get('show_customer_product_code', False):
-            new_res = []
-            for product in self.filtered(
-                    lambda x: x.product_code or x.product_name):
-                name = u"[{}] {}".format(
-                    product.product_code or '', product.product_name or '')
-                new_res.append((product.id, name))
-            if new_res:
-                return new_res
-        return super(ProductSupplierinfo, self).name_get()
+        results = []
+        for suppinfo in self:
+            result = super(ProductSupplierinfo, suppinfo).name_get()
+            for suppinfo_id, name in result:
+                if self.env.context.get('show_customer_product_code'):
+                    names = []
+                    code = (suppinfo.product_code or
+                            suppinfo.product_tmpl_id.default_code or False)
+                    if code:
+                        names.append('[{}]'.format(code))
+                    names.append(suppinfo.product_name or
+                                 suppinfo.product_tmpl_id.name)
+                    if names:
+                        name = ' '.join(names)
+                suppinfo_name = [suppinfo_id, name]
+                results.append(tuple(suppinfo_name))
+        return results
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        if args is None:
+            args = []
+        results = super(ProductSupplierinfo,
+                        self).name_search(name, args, operator, limit)
+        products = self.env['product.product'].search(
+            [('name', operator, name)])
+        domain = ['|', ('product_code', operator, name),
+                  ('product_name', operator, name)]
+        if products:
+            args += expression.OR([domain,
+                                   [('product_tmpl_id', 'in', products.mapped(
+                                     'product_tmpl_id').ids)]])
+        else:
+            args += domain
+        more_results = self.search(args)
+        return more_results and more_results.name_get() or results
--- mrp_repair_partner_lot/tests/test_mrp_repair_partner_lot.py
+++ mrp_repair_partner_lot/tests/test_mrp_repair_partner_lot.py
@@ -8,22 +8,56 @@ class TestMrpRepairPartnerLot(common.TransactionCase):
 
     def setUp(self):
         super(TestMrpRepairPartnerLot, self).setUp()
-        self.supplierinfo = self.env['product.supplierinfo']. search(
-            [], limit=1)
-        self.supplierinfo.write(
-            {'product_code': 'a1a1a1',
-             'product_name': 'AAAAAA'})
+        self.supplierinfo_model = self.env['product.supplierinfo']
+        self.supplier = self.env.ref('base.partner_root')
+        self.product = self.env['product.template'].create({
+            'name': 'Product',
+            'default_code': 'CODE',
+        })
+        self.supplierinfo = self.supplierinfo_model.create({
+            'name': self.supplier.id,
+            'product_tmpl_id': self.product.id,
+            'product_code': 'SUP CODE',
+            'product_name': 'Product Name',
+        })
 
     def test_mrp_repair_partner_lot(self):
-        res = self.supplierinfo.name_get()
+        suppinfo = self.supplierinfo_model.name_search('CODE')
+        self.assertEqual(self.supplierinfo.name_get(), suppinfo)
         self.assertEqual(
-            str(res),
-            "[({}, u'{}')]".format(
-                self.supplierinfo.id, self.supplierinfo.name.name))
-        res = self.supplierinfo.with_context(
-            show_customer_product_code=True).name_get()
+            self.supplierinfo.display_name, self.supplierinfo.name.name)
         self.assertEqual(
-            str(res),
-            "[({}, u'[{}] {}')]".format(
-                self.supplierinfo.id, self.supplierinfo.product_code,
-                self.supplierinfo.product_name))
+            self.supplierinfo.with_context(
+                show_customer_product_code=True).display_name,
+            '[{}] {}'.format(self.supplierinfo.product_code,
+                             self.supplierinfo.product_name))
+        self.supplierinfo.write({
+            'product_code': '',
+            'product_name': 'Product Name',
+        })
+        self.supplierinfo.invalidate_cache()
+        self.assertEqual(
+            self.supplierinfo.with_context(
+                show_customer_product_code=True).display_name,
+            '[{}] {}'.format(self.supplierinfo.product_tmpl_id.default_code,
+                             self.supplierinfo.product_name))
+        self.supplierinfo.write({
+            'product_code': 'SUP CODE',
+            'product_name': '',
+        })
+        self.supplierinfo.invalidate_cache()
+        self.assertEqual(
+            self.supplierinfo.with_context(
+                show_customer_product_code=True).display_name,
+            '[{}] {}'.format(self.supplierinfo.product_code,
+                             self.supplierinfo.product_tmpl_id.name))
+        self.supplierinfo.write({
+            'product_code': '',
+            'product_name': '',
+        })
+        self.supplierinfo.invalidate_cache()
+        self.assertEqual(
+            self.supplierinfo.with_context(
+                show_customer_product_code=True).display_name,
+            '[{}] {}'.format(self.supplierinfo.product_tmpl_id.default_code,
+                             self.supplierinfo.product_tmpl_id.name))
