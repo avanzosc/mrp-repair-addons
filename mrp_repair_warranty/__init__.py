310c63ffda6818b548218eb7371a5f7862992fb2
Daniel-CA <danielcampos@avanzosc.es>
2019-09-25 13:22:33 +0200

    [IMP][8.0] mrp_repair_warranty - Add warranty filter and auto select no invoice repair

--- mrp_repair_warranty/__init__.py
+++ mrp_repair_warranty/__init__.py
@@ -1,5 +1,3 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Daniel Campos - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
---
+++ mrp_repair_warranty/tests/test_mrp_repair_warranty.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Daniel Campos - AvanzOSC
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import openerp.tests.common as common
+
+
+class TestMrpRepairWarranty(common.TransactionCase):
+
+    def setUp(self):
+        super(TestMrpRepairWarranty, self).setUp()
+        self.mrp_repair_model = self.env['mrp.repair']
+        self.product = self.browse_ref('product.product_product_3')
+        self.location_id = self.ref('stock.location_inventory')
+        self.customer = self.env['res.partner'].create({
+            'name': 'Test Customer',
+            'is_company': True,
+            'customer': True,
+            'company_id': self.env.user.company_id.id,
+        })
+        vals = {'name': 'test',
+                'product_id': self.product.id,
+                'location_id': self.location_id,
+                'location_dest_id': self.location_id,
+                'product_uom': self.product.uom_id.id,
+                'partner_id': self.customer.id,
+                'invoice_method': 'after_repair'}
+        self.mrp_repair_customer = self.env['mrp.repair'].create(vals)
+
+    def test_warranty(self):
+        self.assertEqual(self.mrp_repair_customer.invoice_method,
+                         'after_repair')
+        self.mrp_repair_customer.is_in_warranty = True
+        self.mrp_repair_customer.onchange_warranty()
+        self.assertEqual(self.mrp_repair_customer.invoice_method,
+                         'none')
