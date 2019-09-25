d372a34072985009a4180566c7b1eee20e65ddd9
Daniel <danielcampos@avanzosc.es>
2019-09-25 16:15:03 +0200

    [IMP][8.0] mrp_repair_warranty - Add warranty filter and auto select no invoice repair (#61)

--- mrp_repair_warranty/models/mrp_repair.py
+++ mrp_repair_warranty/models/mrp_repair.py
@@ -2,10 +2,15 @@
 # Copyright 2018 Daniel Campos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class MrpRepair(models.Model):
     _inherit = 'mrp.repair'
 
     is_in_warranty = fields.Boolean(string='In warranty')
+
+    @api.onchange('is_in_warranty')
+    def onchange_warranty(self):
+        for repair in self.filtered('is_in_warranty'):
+            repair.invoice_method = 'none'
