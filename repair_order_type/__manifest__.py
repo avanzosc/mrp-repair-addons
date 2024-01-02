4e3e9b0223ce2ec22039963a4f28be89541e0ed1
Unai Beristain <unaiberistain@avanzosc.es>
2024-01-02 15:25:46 +0100

    [MIG] repair_order_type: Migration to 16.0

--- repair_order_type/__manifest__.py
+++ repair_order_type/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Repair Order Type",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing",
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
