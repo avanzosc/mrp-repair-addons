d7327faea425871c23f70a42a7ec614e1970a86e
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-01-05 09:09:54 +0100

    [IMP] repair_validate: pre-commit stuff

--- repair_validate/models/repair_order.py
+++ repair_validate/models/repair_order.py
@@ -1,13 +1,12 @@
 # Copyright 2020 Alfredo de la fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class RepairOrder(models.Model):
-    _inherit = 'repair.order'
+    _inherit = "repair.order"
 
-    finished_task = fields.Boolean(
-        string='Finished task', default=False)
+    finished_task = fields.Boolean(string="Finished task", default=False)
 
     def action_task_end(self):
         for repair in self:
@@ -15,5 +14,4 @@ class RepairOrder(models.Model):
 
     def action_cancel_validation(self):
         for repair in self:
-            repair.write({'finished_task': False,
-                          'state': 'under_repair'})
+            repair.write({"finished_task": False, "state": "under_repair"})
