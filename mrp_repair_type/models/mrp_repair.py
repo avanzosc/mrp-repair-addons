3f31f378f7e7a29bc724e1ee28d664f955b248dd
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-25 15:10:23 +0100

    [MIG] mrp_repair_type: Migration to 16.0

--- mrp_repair_type/models/mrp_repair.py
+++ mrp_repair_type/models/mrp_repair.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Daniel Campos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class MrpRepairType(models.Model):
