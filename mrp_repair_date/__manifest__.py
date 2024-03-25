5b35dce3fb390a4d4049ac174ed61648f5287705
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-25 15:08:40 +0100

    [MIG] mrp_repair_date: Migration to 16.0

--- mrp_repair_date/__manifest__.py
+++ mrp_repair_date/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     'name': 'MRP Repair Date',
-    "version": "8.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": 'AGPL-3',
     "author": 'OdooMRP team,'
               'AvanzOSC,'
--- mrp_repair_date/models/mrp_repair.py
+++ mrp_repair_date/models/mrp_repair.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class MrpRepair(models.Model):
@@ -14,19 +13,16 @@ class MrpRepair(models.Model):
     end_date = fields.Date(
         string='End Date', readonly=True, copy=False)
 
-    @api.multi
     def action_repair_start(self):
         result = super(MrpRepair, self).action_repair_start()
         self.write({'start_date': fields.Date.context_today(self)})
         return result
 
-    @api.multi
     def action_repair_end(self):
         result = super(MrpRepair, self).action_repair_end()
         self.write({'end_date': fields.Date.context_today(self)})
         return result
 
-    @api.multi
     def action_cancel_draft(self):
         result = super(MrpRepair, self).action_cancel_draft()
         self.write({'start_date': False, 'end_date': False})
