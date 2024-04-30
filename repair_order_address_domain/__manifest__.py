489fa9fb2c2ec679a96e983f0f426a99253e0928
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-04-30 14:46:06 +0200

    [ADD] repair_order_address_domain: In repairs remove domain from the "Address" field. (#89)

---
+++ repair_order_address_domain/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Repair Order Address Domain",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es",
+    "depends": [
+        "repair"
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
