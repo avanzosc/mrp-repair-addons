292bbdc32c2babe4c8bd18162a70bfa4cc57da03
Unai Beristain <unaiberis@gmail.com>
2024-05-07 11:42:21 +0200

    [ADD] repair_calendar_view

---
+++ repair_calendar_view/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2016  Agile Business Group
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+
+{
+    'name': 'Repair Calendar View',
+    'version': '12.0.1.1.1',
+    'category': 'Manufacturing',
+    'author': "Agile Business Group, Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/manufacture',
+    'license': 'LGPL-3',
+    'depends': ['repair'],
+    'data': ['views/mrp_repair_view.xml'],
+    'installable': True,
+}
