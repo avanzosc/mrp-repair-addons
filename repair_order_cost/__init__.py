f6a89ee79d2186f58a4bb8bc71c5c9c11b62798d
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-05-20 14:53:22 +0200

    [IMP] repair_order_cost: Carga de datos al instalar el módulo. (#93)

--- repair_order_cost/__init__.py
+++ repair_order_cost/__init__.py
@@ -1,1 +1,15 @@
 from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def _post_install_put_cost_in_repair_orders(cr, registry):
+    """
+    This method will set the production cost on already done manufacturing orders.
+    """
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    repairs = env["repair.order"].search([])
+    for repair in repairs:
+        for operation in repair.operations:
+            operation.onchange_product_id()
+        for line in repair.fees_lines:
+            line.onchange_product_id()
