463871db1d543d31d4815b0a9a9ea49b53ca72ba
Alfredo <alfredodelafuente@avanzosc.es>
2024-11-26 08:20:27 +0100

    [14.0][ADD] Catch custmomer from lot wehicle.

---
+++ repair_order_lot_vehicle_customer/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Repair Order Lot Vehicle Customer",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
+    "depends": [
+        "fleet",
+        "repair",
+        "stock_production_lot_fleet_vehicle",
+    ],
+    "data": [],
+    "installable": True,
+}
