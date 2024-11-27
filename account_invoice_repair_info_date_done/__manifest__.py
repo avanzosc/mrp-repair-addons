d917158f0d787b18afa01606317592bb130fe715
Alfredo <alfredodelafuente@avanzosc.es>
2024-11-27 14:15:53 +0100

    [14.0][ADD] account_invoice_repair_info_date_done: In invoice report print repair date done.

---
+++ account_invoice_repair_info_date_done/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice Repair Info Date Done",
+    "version": "14.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
+    "depends": ["account_invoice_repair_info", "repair_date_done"],
+    "data": [
+        "report/account_invoice_report.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
