5fdf3210ac8af9a56d0ff228145c956e72d29017
Unai Beristain <unaiberis@gmail.com>
2025-02-10 16:36:13 +0100

    [IMP] account_invoice_section_repair: Improve error

--- account_invoice_section_repair/models/account_move.py
+++ account_invoice_section_repair/models/account_move.py
@@ -14,46 +14,40 @@ class AccountMove(models.Model):
     def create(self, vals_list):
         if "from_repair_order" not in self.env.context:
             return super().create(vals_list)
-        repairs_processed = self.env["repair.order"]
-        pending_fee = True
+        my_invoice_line_ids = []
+        repairs_treated = self.env["repair.order"]
+        fee_pending = True
         for vals in vals_list:
-            updated_invoice_lines = []
-            invoice_lines = vals.get("invoice_line_ids", [])
-            for line_vals in invoice_lines:
-                line_data = line_vals[2]
-                if "repair_line_ids" in line_data:
+            lines_vals = vals.get("invoice_line_ids")
+            for line_vals in lines_vals:
+                if "repair_line_ids" in line_vals[2]:
                     repair_line = self.env["repair.line"].browse(
-                        line_data["repair_line_ids"][0][1]
+                        line_vals[2].get("repair_line_ids")[0][1]
                     )
-                    repair_order = repair_line.repair_id
-                    if repair_order not in repairs_processed:
-                        pending_fee = True
-                        repairs_processed += repair_order
-                        updated_invoice_lines = self._prepare_repair_info_section(
-                            repair_order,
-                            updated_invoice_lines,
-                            add_fee=False,
-                            print_repair_info=True,
+                    if repair_line.repair_id not in repairs_treated:
+                        fee_pending = True
+                        repairs_treated += repair_line.repair_id
+                        my_invoice_line_ids = self._prepare_repair_info_section(
+                            repair_line.repair_id, my_invoice_line_ids, False, True
                         )
-                elif "repair_fee_ids" in line_data:
+                elif "repair_fee_ids" in line_vals[2]:
                     repair_fee = self.env["repair.fee"].browse(
-                        line_data["repair_fee_ids"][0][1]
+                        line_vals[2].get("repair_fee_ids")[0][1]
                     )
-                    repair_order = repair_fee.repair_id
                     print_repair_info = False
-                    if (repair_order not in repairs_processed) or pending_fee:
-                        if repair_order not in repairs_processed:
+                    if (repair_fee.repair_id not in repairs_treated) or fee_pending:
+                        if repair_fee.repair_id not in repairs_treated:
                             print_repair_info = True
-                            repairs_processed += repair_order
-                        pending_fee = False
-                        updated_invoice_lines = self._prepare_repair_info_section(
-                            repair_order,
-                            updated_invoice_lines,
-                            add_fee=True,
-                            print_repair_info=print_repair_info,
+                            repairs_treated += repair_fee.repair_id
+                        fee_pending = False
+                        my_invoice_line_ids = self._prepare_repair_info_section(
+                            repair_fee.repair_id,
+                            my_invoice_line_ids,
+                            True,
+                            print_repair_info,
                         )
-                updated_invoice_lines.append(line_vals)
-            vals["invoice_line_ids"] = updated_invoice_lines
+                my_invoice_line_ids.append(line_vals)
+            vals["invoice_line_ids"] = my_invoice_line_ids
         return super().create(vals_list)
 
     def _prepare_repair_info_section(
@@ -89,28 +83,22 @@ class AccountMove(models.Model):
         return vals
 
     def _get_repair_name_for_values(self, repair):
-
         date_repair = self._convert_to_local_date(repair.date_repair, repair.user_id)
-
         repair_name = _("Repair: %(repair)s, Date: %(date)s") % {
             "repair": repair.name,
             "date": date_repair,
         }
-
         if repair.lot_id:
             repair_name = _("%(repair_name)s, Num. Serie: %(lot)s") % {
                 "repair_name": repair_name,
                 "lot": repair.lot_id.name,
             }
-
         if repair.quotation_notes:
             quotation_notes = repair.convert_html_notes_to_char(repair.quotation_notes)
-
             repair_name = _("%(repair_name)s\n%(notes)s") % {
                 "repair_name": repair_name,
                 "notes": quotation_notes,
             }
-
         return repair_name
 
     def _convert_to_local_date(self, mydate, user):
