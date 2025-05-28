0c4c1030e2caff5f46c487a4262e29f21bcefed6
luciaavanzosc <luciaavanzosc@gmail.com>
2025-05-28 10:38:49 +0200

    [16.0][ADD] repair_fee_menu: Creates a menu for repair fee operations.

---
+++ repair_fee_menu/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Aner Arregi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Repair Fee Menu",
+    "author": "AvanzOSC",
+    "version": "16.0.1.0.0",
+    "category": "Repairs/Fee",
+    "summary": "Creates a menu for repair fee operations.",
+    "data": [
+        "views/repair_fee_menu_views.xml",
+    ],
+    "depends": ["repair"],
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
+    "installable": True,
+    "license": "LGPL-3",
+}
