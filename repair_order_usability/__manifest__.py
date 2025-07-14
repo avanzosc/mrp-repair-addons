2b4bfbbb3ffdf3a3ad78a20b24ddb6614c2cd1d3
Alfredo <alfredodelafuente@avanzosc.es>
2025-07-14 08:14:07 +0200

    [16.0][ADD] repair_order_usability: In Repair Order tree view show "Tags" field.

---
+++ repair_order_usability/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Repair Order Usability",
+    "version": "16.0.1.1.0",
+    "category": "Inventory/Inventory",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "repair",
+    ],
+    "data": [
+        "views/repair_order_views.xml",
+    ],
+    "installable": True,
+}
