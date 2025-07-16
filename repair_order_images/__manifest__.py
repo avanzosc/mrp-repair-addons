66ff565cdac407d6b97b241951be75dc9f6837bd
Alfredo <alfredodelafuente@avanzosc.es>
2025-07-16 11:21:15 +0200

    [16.0][ADD] repair_order_images: In Repair Orders new fields: Photo1, Photo2, Photo3, and Photo4.

---
+++ repair_order_images/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Repair Order Images",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "website": "https://github.com/avanzosc/mrp-repair-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "depends": ["repair", "repair_order_extra_data"],
+    "data": ["views/repair_order_views.xml", "reports/repair_order_report.xml"],
+    "installable": True,
+}
