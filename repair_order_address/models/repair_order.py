a8140df27e262fdca6963ceaca700938f1420430
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-03 07:57:38 +0100

    [18.0][ADD] repair_order_address: In repair order new field "address".

---
+++ repair_order_address/models/repair_order.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class RepairOrder(models.Model):
+    _inherit = "repair.order"
+
+    address_id = fields.Many2one(
+        string="Delivery Address",
+        comodel_name="res.partner",
+        domain="[('parent_id','=',partner_id)]",
+        check_company=True,
+        states={"confirmed": [("readonly", True)]},
+    )
