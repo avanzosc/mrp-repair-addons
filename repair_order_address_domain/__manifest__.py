10094a4356c4c2f2b3f970a7694073a1a20953e7
Alfredo <alfredodelafuente@avanzosc.es>
2025-12-23 12:05:55 +0100

    [18.0][MIG] repair_order_address_domain: Migration to 18.0

--- repair_order_address_domain/__manifest__.py
+++ repair_order_address_domain/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Repair Order Address Domain",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Inventory/Inventory",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/mrp-repair-addons",
-    "depends": ["repair"],
+    "depends": ["repair_order_address"],
     "data": [],
     "installable": True,
 }
