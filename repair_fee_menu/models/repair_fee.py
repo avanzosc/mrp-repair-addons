b236599d52149e75c0f144fe8ad7c04f3b91b2ec
Oihane Crucelaegui <oihanecruce@gmail.com>
2026-02-20 16:39:15 +0100

    [IMP] repair_fee_menu: remove unnecessary code

--- repair_fee_menu/models/repair_fee.py
+++ repair_fee_menu/models/repair_fee.py
@@ -7,17 +7,12 @@ from odoo import fields, models
 class RepairFee(models.Model):
     _inherit = "repair.fee"
 
-    def _get_selection_repair_state(self):
-        return self.env["repair.order"].fields_get(allfields=["state"])["state"][
-            "selection"
-        ]
-
     repair_state = fields.Selection(
-        selection=_get_selection_repair_state,
         related="repair_id.state",
         store=True,
     )
     repair_create_date = fields.Datetime(
+        string="Repair Create Date",
         related="repair_id.create_date",
         store=True,
     )
